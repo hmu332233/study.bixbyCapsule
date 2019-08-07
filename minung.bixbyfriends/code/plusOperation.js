2802015fb25824431465a218c0d1bd1280dce454
minung.han <hmu332233@gmail.com>
2019-08-07 15:27:23 +0000

    feat(code): 비즈니스 로직 및 endpoint 추가

--- minung.bixbyfriends/code/plusOperation.js
+++ minung.bixbyfriends/code/plusOperation.js
@@ -1,3 +1,12 @@
-module.exports.function = function plusOperation (arg1, arg2, ...) {
-  return {}
+module.exports.function = function plusOperation (leftOperand, rightOperand) {
+  const console = require('console');
+  
+  let name = '더하기';
+  let result = leftOperand + rightOperand;
+  console.log('name: ' + name);
+  console.log('result: ' + result);
+  return {
+    result: result,
+    operatorName: name,
+  };
 }
